b055fc9ef9a29d0f1e5ebddf789676d55bca38ca
PABLO ALVAREZ FERNANDEZ <117449906+EldandydeBarcelona@users.noreply.github.com>
2025-11-21 17:18:53 +0100

    Initial commit

---
+++ src/main/java/ExamenMocPAF/ExamenMocPAF/ExamenMocPafApplication.java
@@ -0,0 +1,13 @@
+package ExamenMocPAF.ExamenMocPAF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMocPafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMocPafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ExamenMocPAF/ExamenMocPAF/ExamenMocPafApplicationTests.java
@@ -0,0 +1,13 @@
+package ExamenMocPAF.ExamenMocPAF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenMocPafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
